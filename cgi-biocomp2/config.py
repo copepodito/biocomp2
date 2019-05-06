5dcc56d27793160774f7698901f9d377524aa868
Diego <48446612+copepodito@users.noreply.github.com>
2019-05-06 14:01:24 +0100

    Update config.py

--- cgi-biocomp2/config.py
+++ cgi-biocomp2/config.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 
+index_path = "http://student.cryst.bbk.ac.uk/~cd002/index.html"
+details_accession_path = "http://student.cryst.bbk.ac.uk/cgi-bin/cgiwrap/cd002/details.py?option=accession&query="
+favicon_path = "http://student.cryst.bbk.ac.uk/~cd002/favicon.ico"
+logo_path = "http://student.cryst.bbk.ac.uk/~cd002/logo.svg"
+microarray_fig_path = "http://student.cryst.bbk.ac.uk/~cd002/microarray.jpg"
+mysql_db_login = 'mysql -u tg001 -p"f29z70k#r" tg001'
+
+
+# Instructions for Georgina-----------------------------------
+# Acces cgi-biocomp2, where config.py file is
+import sys
+sys.path.insert(0, "../cgi-biocomp2/")
+
+# Import the config.py itself
+import config
+
+# This is how you use the variable
+config.mysql_db_login
